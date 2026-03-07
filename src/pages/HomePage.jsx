a050543f894d5542d8a08229da496c240d007f7f
TharsikanV <108121067+TharsikanV@users.noreply.github.com>
2026-03-07 23:11:08 +0530

    Add Results Panel

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import { useSelector } from 'react-redux';
 import ModeNavigation from '../components/ModeNavigation';
 import ImageAnalysis from '../components/ImageAnalysis';
 import ClinicalDataAnalysis from '../components/ClinicalDataAnalysis';
+import ResultsPanel from '../components/ResultsPanel';
 
 function HomePage() {
   const { mode, results } = useSelector((state) => state.analysis);
@@ -17,6 +18,9 @@ function HomePage() {
         </div>
 
         {/* Right Column: Results and Chat */}
+         <div className="space-y-6">
+          {results && <ResultsPanel />}
+        </div>
       </div>
     </div>
   );
