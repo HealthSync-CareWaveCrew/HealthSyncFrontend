a8d7bc3283fbde62e235990969731beda091f3bc
TharsikanV <108121067+TharsikanV@users.noreply.github.com>
2026-03-07 21:43:51 +0530

    Add Text based prediction ui and integration

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 import ModeNavigation from '../components/ModeNavigation';
 import ImageAnalysis from '../components/ImageAnalysis';
+import ClinicalDataAnalysis from '../components/ClinicalDataAnalysis';
 
 function HomePage() {
   const { mode, results } = useSelector((state) => state.analysis);
@@ -12,7 +13,7 @@ function HomePage() {
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
         {/* Left Column: Input Forms */}
         <div>
-          {mode === 'image' ? <ImageAnalysis /> : <ImageAnalysis />}
+          {mode === 'image' ? <ImageAnalysis /> : <ClinicalDataAnalysis />}
         </div>
 
         {/* Right Column: Results and Chat */}
