058fc1d98f081a53a888953a2d7ffe7d51aa13b6
poojak <139669968+pooja180801@users.noreply.github.com>
2026-03-09 10:46:19 +0530

    feat: add admin reducer to Redux store

--- src/Redux/Store/store.js
+++ src/Redux/Store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import analysisReducer from '../Features/analysisSlice';
 import chatReducer from '../Features/chatSlice';
 import authReducer from '../Features/authSlice';
+import adminReducer from '../Features/adminSlice';
 
 export const store = configureStore({
   reducer: {
     analysis: analysisReducer,
     chat: chatReducer,
     auth: authReducer,
+    admin: adminReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
