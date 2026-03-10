d1c2cdf74717cb31810b75a7d2e314edbcd6bd9b
poojak <139669968+pooja180801@users.noreply.github.com>
2026-03-10 09:35:04 +0530

    Add Google OAuth provider integration to main application entry point

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,17 @@ import { Provider } from 'react-redux'
 import { store } from './Redux/Store/store'
 import './index.css'
 import AllRoutes from './routes/AllRoutes'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+console.log('Google Client ID:', GOOGLE_CLIENT_ID);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider store={store}>
-      <AllRoutes />
-    </Provider>
-  </StrictMode>,
-)
+  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+    <StrictMode>
+      <Provider store={store}>
+        <AllRoutes />
+      </Provider>
+    </StrictMode>
+  </GoogleOAuthProvider>
+);
